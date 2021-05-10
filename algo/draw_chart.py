2c688b1b2b965326d69f18bdf6c0780e7f5d22fd
kenny-chen <kennychan@dbpower.com.hk>
2021-05-10 14:46:25 +0800

    Updated data

--- algo/draw_chart.py
+++ algo/draw_chart.py
@@ -58,8 +58,8 @@ for k, _df in data1.items():
     df["Volume"] = pd.to_numeric(df["Volume"])
     df.index = pd.to_datetime(df.sdate)
     df = df.tail(no_days)
-    prop = fm.FontProperties(fname='D:/PycharmProjects/quant_trading_1/msjh.ttf')
-    style = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size':12, 'font.family' : 'SimHei'}, gridaxis='both')
+    prop = fm.FontProperties(fname='D:/PycharmProjects/scrapy-001/algo/msjh.ttf')
+    style = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size':12, 'font.family' : 'SimSun'}, gridaxis='both')
 
     if _df['sdate'].shape[0] > 0:
         stime = _df['sdate'].iloc[-1]
